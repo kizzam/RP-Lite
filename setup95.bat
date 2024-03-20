fc77096252d6ffb3abf57b5467c77ce4e2690a65
kizzam <rpms2002@hotmail.com>
2024-03-20 14:07:06 +1000

    Add files via upload

---
+++ setup95.bat
@@ -0,0 +1,8 @@
+echo ONLY RUN IF INSTRUCTED
+pause
+copy a:*.ini c:\windows\*.ini
+COPY a:*.lic c:\windows\*.lic
+copy a:*.lnk c:\windows\desktop\*.lnk
+copy a:*.vbx c:\windows\system\*.vbx
+echo
+pause
